fda152bd72f5cc4d71c87abc12d05e70a80279a4
Teatree <4aynoedepebo@gmail.com>
2022-05-26 06:01:38 -0700

    repeated content fix

--- bot.py
+++ bot.py
@@ -11,29 +11,29 @@ creds = f.read()
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-url = "https://grabfreegames.com/"
+def getTitles(result):
+    url = "https://grabfreegames.com/"
 
-try:
-   page = urlopen(url)
-except:
-   print("Error opening the URL")
+    try:
+       page = urlopen(url)
+    except:
+       print("Error opening the URL")
 
-soup = BeautifulSoup(page, 'html.parser')
+    soup = BeautifulSoup(page, 'html.parser')
 
-content = soup.find('div', {"class": "freebies row break-on-4"})
+    content = soup.find('div', {"class": "freebies row break-on-4"})
 
-result = ''
-for i in content.findAll('div'):
-    for x in i.findAll('div', {"class": "free-title"}):
-        for a in x.findAll('a', href=True):
-            if 'Free Steam Game' in x.text:
-                result = result + ' ' +  x.text + '    -    ' + str(a['href'])
-            if 'Free Epic Games Game' in x.text:
-                result = result + ' ' +  x.text + '    -    ' + str(a['href'])
-            if 'Free GOG Game' in x.text:
-                result = result + ' ' +  x.text + '    -    ' + str(a['href'])
-
-print(result)
+    for i in content.findAll('div'):
+        for x in i.findAll('div', {"class": "free-title"}):
+            for a in x.findAll('a', href=True):
+                if 'Free Steam Game' in x.text:
+                    result = result + ' ' +  x.text + '    -    ' + str(a['href'])
+                if 'Free Epic Games Game' in x.text:
+                    result = result + ' ' +  x.text + '    -    ' + str(a['href'])
+                if 'Free GOG Game' in x.text:
+                    result = result + ' ' +  x.text + '    -    ' + str(a['href'])
+                    
+    return str(result)
 
 ###########
 
@@ -54,13 +54,13 @@ target_channel_idMIN = 936999636625940540
 async def called_once_a_dayXOX():
     message_channel = bot.get_channel(target_channel_idXOX)
     print(f"Got channel {message_channel}")
-    await message_channel.send(result)
+    await message_channel.send(getTitles("xox"))
 
 @tasks.loop(hours=168)
 async def called_once_a_dayMIN():
     message_channel = bot.get_channel(target_channel_idMIN)
     print(f"Got channel {message_channel}")
-    await message_channel.send(result)
+    await message_channel.send(getTitles("min"))
 
 @called_once_a_dayXOX.before_loop
 async def before():
